3793a2e7b0060e5a170e4467e94b8c523f57d345
AimanHaidar <atahalabsi@gmail.com>
2025-09-29 23:44:36 +0300

    finish first node of hand way and test it

--- src/maze_solver/src/one_hand_maze_solver.cpp
+++ src/maze_solver/src/one_hand_maze_solver.cpp
@@ -72,48 +72,60 @@ Maze maze = {
 
 using namespace std::chrono_literals;
 
-class IsWallHandOpen : public BT::StatefulActionNode{
+class IsWallHandWayOpen : public BT::StatefulActionNode{
 public:
-    IsWallHandOpen(const std::string& name, const BT::NodeConfiguration& config)
+    IsWallHandWayOpen(const std::string& name, const BT::NodeConfiguration& config)
         : BT::StatefulActionNode(name, config) {
-            current_position = config.blackboard->get<Position>("current_position");
+            this->config = config;
         }
 
-    BT::NodeStatus onStart() override {
-        std::string wall_hand_;
-        getInput<std::string>("wall_hand", wall_hand_);
-        if (wall_hand_=="left") {
-            wall_hand_ = LEFT;
-        } else if (wall_hand_=="right") {
-            wall_hand_ = RIGHT;
+    BT::NodeStatus onStart() {
+        current_position = config.blackboard->get<Position>("current_position");
+        std::string wall_hand_str;
+        getInput<std::string>("wall_hand", wall_hand_str);
+        if (wall_hand_str == "left") {
+            this->wall_hand_ = LEFT;
+        } else if (wall_hand_str == "right") {
+            this->wall_hand_ = RIGHT;
         } else {
             throw std::runtime_error("Invalid wall_hand input. Use 'left' or 'right'.");
         }
 
-        return BT::NodeStatus::SUCCESS;
+        return BT::NodeStatus::RUNNING;
     }
 
     BT::NodeStatus onRunning(){
         // next position toward the wall hand
         Position nextPosition = getNextPosition(current_position, relativeDirection(current_position, wall_hand_));
         if(maze[nextPosition.y][nextPosition.x] == PATH) {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Wall hand way %s is open", wall_hand_ == LEFT ? "left" : "right");
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Wall hand way is open");
             move(current_position, relativeDirection(current_position, wall_hand_));
+            config.blackboard->set("current_position", current_position);
             return BT::NodeStatus::SUCCESS;
         } else {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Wall hand %s is closed");
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Wall hand is closed");
             return BT::NodeStatus::FAILURE;
         }
+    }
+
+    void onHalted() { return; }
+
+    static BT::PortsList providedPorts() {
+      return {
+        BT::InputPort<std::string>("wall_hand"),
+      };
     }
 
 private:
     Direction wall_hand_;
     Position current_position;
+    BT::NodeConfiguration config;
 };
 
 class HandMazeSolver : public rclcpp::Node {
 public:
     HandMazeSolver() : Node("hand_maze_solver") {
+        RCLCPP_INFO(this->get_logger(), "Starting One Hand Maze Solver Node");
         first_ = true;
         timer_ = this->create_wall_timer( 0.5s, std::bind(&HandMazeSolver::tick_function, this));
         blackboard_ = BT::Blackboard::create();
@@ -127,7 +139,8 @@ private:
         blackboard_->set<Position>("current_position", initial_position);
         blackboard_->set<Maze>("maze", maze);
         // TODO  set tree
-        this->declare_parameter<std::string>("tree_xml_file", "");
+        factory_.registerNodeType<IsWallHandWayOpen>("IsWallHandWayOpen");
+        this->declare_parameter<std::string>("tree_xml_file", "/home/aymanhadair/ROS2/BahaviorTree/src/maze_solver/trees/one_hand_maze_solver.xml");
         std::string tree_file;
         this->get_parameter("tree_xml_file", tree_file);
         tree_ = factory_.createTreeFromFile(tree_file, blackboard_);
@@ -136,9 +149,10 @@ private:
 
     void tick_function() {
       if( first_) {
+        RCLCPP_INFO(this->get_logger(), "Initializing Behavior Tree");
           init_btree();
           first_ = false;
-      } 
+      }
       tree_.tickOnce();
     }
     BT::BehaviorTreeFactory factory_;
@@ -147,3 +161,15 @@ private:
     bool first_;
     BT::Tree tree_;
 };
+
+int main(int argc, char * argv[]) {
+
+  rclcpp::init(argc, argv);
+
+  rclcpp::Node::SharedPtr BT_executor_node = std::make_shared<HandMazeSolver>();
+  rclcpp::spin( BT_executor_node );
+  rclcpp::shutdown();
+
+  return 0;
+
+}
